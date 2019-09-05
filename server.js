e7c14f3e70c75bed4822185e0d873d2c4587a3c3
Sean Ryan <ryan.sean@acmhq.acm.org>
2019-09-05 16:28:15 -0400

    reconfigured server port

--- server.js
+++ server.js
@@ -1,13 +1,36 @@
 const express = require('express');
 const app = express();
 const fetch = require('node-fetch');
+const http = require('http');
 
 const base = 'https://api.themoviedb.org/3';
 const api_key = process.env.API_KEY;
 require('dotenv').config();
 
+const port = normalizePort(process.env.PORT || "3000");
+app.set("port", port);
 
-app.listen(3000, () => console.log('listening on port 3000!'));
+const server = http.createServer(app);
+
+server.listen(port);
+
+function normalizePort(val) {
+    const port = parseInt(val, 10);
+    if (isNaN(port)) {
+      return val;
+    }
+    if (port >= 0) {
+      return port;
+    }
+    return false;
+  }
+
+  server.on("listening", () => {    
+    console.log(`server is listening for requests on port ${server.address().port}`);
+  });
+
+
+// app.listen(3000, () => console.log('listening on port 3000!'));
 app.use(express.static('public'));
 app.use(express.json({ limit: '1mb' }));
 
